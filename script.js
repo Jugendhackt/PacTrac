c30e851d9fd4b39ddb76bf7d7c3233c78ba302a1
Ben F <ben@realeaz.de>
2019-06-15 16:49:27 +0200

    ausgabe auf deutsch

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function request() {
-    var requestURL = 'https://api-eu.dhl.com/track/shipments?trackingNumber=' + document.getElementById('delivery').value;
+    var requestURL = 'https://api-eu.dhl.com/track/shipments?trackingNumber=' + document.getElementById('delivery').value + "&language=de";
     fetch(requestURL,
         {headers: new Headers({'DHL-API-Key': 'MAy2ckLSa0w0xgxJtgrXToRWOATR41DE'})})
         .then(function (response) {
             return response.json()
         }).then(function (json) {
         console.log(json)
+        var herkunft = json.shipments[0].destination.address.addressLocality;
+        var ziel = json.shipments[0].origin.address.addressLocality;
+        var status_zeit = json.shipments[0].status.timestamp;
+        var status_beschreibung = json.shipments[0].status.description;
     });
 }
