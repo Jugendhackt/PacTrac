ab49888cf30a1f0f57efc4c68427f9e368a1d27b
Ben F <ben@realeaz.de>
2019-06-15 21:25:41 +0200

    fixed loading screen

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ function request() {
     var requestURL = 'https://api-eu.dhl.com/track/shipments?trackingNumber=' + delivery.value + "&language=de";
 
     var loading = document.getElementById('loading');
+    document.getElementById('logo').src = "./Logos/PacTraclogokleinfuerweb01.png";
     loading.style.display = "flex";
     loading.style.justifyContent = "center";
-    loading.style.transition = "display 0.3s";
     document.body.style.backgroundColor = "#facd2e";
-    document.body.style.transition = "background-color .3s";
     document.getElementById("shipment_progress").style.opacity = 0;
     document.getElementById("shipment_progress").style.transition = "none";
 
@@ -21,7 +20,7 @@ function request() {
         var ziel = json.shipments[0].origin.address.addressLocality;
         var status_zeit = json.shipments[0].status.timestamp;
         var status_beschreibung = json.shipments[0].status.description;
-
+        document.getElementById('logo').src = "./Logos/Pactraclogoklein00.png";
         loading.style.display = "none";
         document.body.style.backgroundColor = "#2171cc";
         document.getElementById("shipment_progress").style.opacity = 1;
